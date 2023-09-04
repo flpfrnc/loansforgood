0bdbfa6b0716ccb7970b0be5b1f511b79caec8e8
Felipe França <felipevictor67@gmail.com>
2023-09-04 19:57:40 -0300

    fix: add approved loan proposals to admin management

--- backend/loansforgood/admin.py
+++ backend/loansforgood/admin.py
@@ -1,5 +1,18 @@
+from .models import LoanProposal
 from django.contrib import admin
-from .models import FormField
-# Register your models here.
+from .models import FormField, LoanProposal
 
 admin.site.register(FormField)
+
+
+class ApprovedLoanProposalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'document', 'status',
+                    'approved', 'created_at', 'updated_at')
+    list_filter = ('status', 'approved')
+    search_fields = ('name', 'document')
+
+    def get_queryset(self, request):
+        return LoanProposal.objects.filter(approved=True)
+
+
+admin.site.register(LoanProposal, ApprovedLoanProposalAdmin)
